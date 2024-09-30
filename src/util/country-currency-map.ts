27bf23a77939aea7d5f6d0b557b674b96161921a
starline0008 <krishna.sharma@hotmailcom>
2024-09-30 12:08:55 +0530

    removed the support for inr

--- src/util/country-currency-map.ts
+++ src/util/country-currency-map.ts
@@ -2,7 +2,7 @@ import Currency from "@/enum/currency-enum";
 
 const countryCurrencyMap: Record<string, Currency> = {
   US: Currency.USD,
-  IN: Currency.INR,
+  //IN: Currency.INR,
   AE: Currency.AED,
   // Add more country-to-currency mappings as required
 };
