a7d7ae8979b405f6c8f909e318af73d571b89f2e
krishnasharma0008 <krishna.sharma@hotmail.com>
2024-12-21 15:14:41 +0530

    #

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3001",
+      args: "start -p 3002",
     },
   ],
 };
